1cdf5751318714cfe50bc51898f8ec91ef24bc55
Andew Schultz <andrew7primary@gmail.com>
2022-11-15 18:31:42 -0700

    docker update

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,40 +1,18 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from events.api_views import ConferenceListEncoder, Conference
-from .models import Attendee
+from .models import Attendee, ConferenceVO
 import json
 from django.views.decorators.http import require_http_methods
+
+
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
 
 
 class AttendeesListEncoder(ModelEncoder):
     model = Attendee
     properties = ["name"]
-
-
-@require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
-    if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
-        return JsonResponse(
-            {"attendees": attendees},
-            encoder=AttendeesListEncoder,
-        )
-    else:
-        content = json.loads(request.body)
-        try:
-            conference = Conference.objects.get(id=conference_id)
-            content["conference"] = conference
-        except Conference.DoesNotExist:
-            return JsonResponse(
-                {"message": "Invalid conference id"},
-                status=400,
-            )
-        attendee = Attendee.objects.create(**content)
-        return JsonResponse(
-            attendee,
-            encoder=AttendeeEncoder,
-            safe=False,
-        )
 
 
 class AttendeeEncoder(ModelEncoder):
@@ -47,8 +25,36 @@ class AttendeeEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder(),
+        "conference": ConferenceVODetailEncoder(),
     }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_attendees(request, conference_vo_id=None):
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        return JsonResponse(
+            {"attendees": attendees},
+            encoder=AttendeesListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            conference_href = f"/api/conferences/{conference_vo_id}/"
+            conference = ConferenceVO.objects.get(import_href=conference_href)
+            content["conference"] = conference
+        except ConferenceVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeEncoder,
+            safe=False,
+        )
 
 
 @require_http_methods(["DELETE", "GET", "PUT"])
